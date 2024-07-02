98fd2e6aee4619ce694a1af84141b4a5b2bb0e62
Tuavsn <hoctuavsn@gmail.com>
2024-07-02 17:53:27 +0700

    first setup

---
+++ user-service/src/main/java/com/e_commerce/user_service/entity/Role.java
@@ -0,0 +1,8 @@
+package com.e_commerce.user_service.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+@NoArgs
+public class Role {
+}
---
+++ user-service/src/main/java/com/e_commerce/user_service/entity/User.java
@@ -0,0 +1,4 @@
+package com.e_commerce.user_service.entity;
+
+public class User {
+}
